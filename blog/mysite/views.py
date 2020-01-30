2959f00104aeb047320b448570c8b0a8da9c253c
Ray <ottotruepro@gmail.com>
2020-01-30 13:59:45 +0700

    part 3 views and templates

--- blog/mysite/views.py
+++ blog/mysite/views.py
@@ -1,12 +1,33 @@
-from django.shortcuts import render
+from django.http import Http404
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from .models import Question
 
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, dude.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return render(request, 'mysite/index.html', context)
 
 
 def testpage(request):
     return HttpResponse("tetstpage is here")
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'mysite/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    response = f'Ты смотришь на результаты опроса {question_id}'
+    return HttpResponse(response)
+
+
+def vote(request, question_id):
+    return HttpResponse(f'Ты отвечаешь на опрос {question_id}')
