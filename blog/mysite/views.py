881cd02ebe2d52ed9c6ce119c2d87cc884cbdd36
Ray <ottotruepro@gmail.com>
2020-01-31 22:21:46 +0700

    part 4 forms and generic views

--- blog/mysite/views.py
+++ blog/mysite/views.py
@@ -1,33 +1,45 @@
-from django.http import Http404
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views import generic
 
-from .models import Question
+from .models import Question, Choice
 
 
 # Create your views here.
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list
-    }
-    return render(request, 'mysite/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'mysite/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
 
 def testpage(request):
     return HttpResponse("tetstpage is here")
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'mysite/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'mysite/detail.html'
 
 
-def results(request, question_id):
-    response = f'Ты смотришь на результаты опроса {question_id}'
-    return HttpResponse(response)
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'mysite/results.html'
 
 
 def vote(request, question_id):
-    return HttpResponse(f'Ты отвечаешь на опрос {question_id}')
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        # Вот этот ['choice'] это атрибут name="" в шаблоне detail
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Снова показываем форму с вопросом и выборами ответов
+        return render(request, 'mysite/detail.html', {'question': question, 'error_message': "Вариант ответа выбери, э!"})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('mysite:results', args=(question_id,)))
